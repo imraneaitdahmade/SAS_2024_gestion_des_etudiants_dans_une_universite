ae3e6b2d8f82be3b29e05864e435b858d984e74e
imraneaitdahmade <imraneaitdahmade@gmail.com>
2024-09-11 10:24:05 +0100

    ajoute la fonction de recherche

--- sas.c
+++ sas.c
@@ -12,6 +12,7 @@ typedef struct
 } eleve;
 // declaration des variables global //
 eleve c[100];
+ const char listdpar[6][6]={"MIP","PC","BIO","IN"};
 int q;
 // la fonction d ajoute//
 void ajout()
@@ -171,18 +172,145 @@ else if(p==1){
        if(strcmp(c[d].departement,moydepartement)==0){
               s=s+c[d].note;
               f++;
+              printf("%d",c[d].note);
        }
        }
        M=s/f;
-       printf("le moyenne general dans la departement %s est %.0f",moydepartement,M);
-
+       printf("le moyenne general dans la departement %s est %f",moydepartement,M);
+     }
+}
+//fonction de recherche //
+void recherche(){
+     //declaration des variables local de fonction recherche //
+     int j,k,l;
+     char rechname[20],rechdepartement[20] ;
+     //menu de recherche//
+     printf("______________________________________________________________________________________________________________\n");
+     printf("--------------------------------------------------rechercher--------------------------------------------------\n");
+     printf("   1)recherche  un etudiant par son nom                         2)Afficher la liste des etudiants  inscrits dans un departement spécifique\n \n");
+     printf("______________________________________________________________________________________________________________\n");
+     printf("entre :");
+     scanf("%d",&j);
+     switch(j){
+       case 1:
+       printf("entre le nom d etudiant :");
+       getchar();
+       fgets(rechname, 20, stdin);
+       for(k=0;k<q;k++){
+              if(strcmp(rechname,c[k].nom)){
+       printf("les informationsde eleve id:%d",k+1)  ;
+       printf("le nom est :%s \n",c[k].nom);
+       printf("le prenom est :%s \n",c[k].prenom);
+       printf("le date de naissance est :%s \n",c[k].date);
+       printf("la departement :%s \n",c[k].departement);
+       printf("le nombre total:%.2f\n",c[k].note);
+              }
+}
+       break;
+       case 2:
+       printf("entre le departement :");
+       scanf("%s",rechdepartement);
+       for(l=0;l<q;l++){
+       if(strcmp(rechdepartement,c[l].departement)){
+       printf("les eleves de departement %s sont :\n",rechdepartement);
+       printf("les informationsde eleve id:%d \n",l+1)  ;
+       printf("le nom est :%s \n",c[l].nom);
+       printf("le prenom est :%s \n",c[l].prenom);
+       printf("le date de naissance est :%s \n",c[l].date);
+       printf("la departement :%s \n",c[l].departement);
+       printf("le nombre total:%.2f\n",c[l].note);
+       break;
+       }
+       }
      }
 }
 int main()
 {
-       ajout();
-       modification();
-       affichage();
+       //declaration des variables local de main
+       int g,h;
+       //menu principal//
+       menu:
+       printf("______________________________________________________________________________________________________________\n");
+       printf("--------------------------------------gestion des etudiants dans une universite----------------------------\n");
+       printf("   1)ajouter un etudiant                                                        2)modifier les informations \n \n");
+       printf("   3)suprimer un etudiant                                                       4)afficher les informations des etudiants \n \n");
+       printf("   5)rechercher un etudiant                                                     6)Trier les etudiants \n \n");
+       printf("   7)statistiques                                                                8)calculer le moyenne general \n \n");
+       printf("______________________________________________________________________________________________________________\n");
+       printf("entre :");
+       scanf("%d",&g);
+       switch(g){
+        case 1:
+        ajout();
+          printf("click 1 pour retourne au menu \n click 2 pour finir le programme\n ");
+        printf("entre :");
+        scanf("%d",&h);
+        if(h==1)
+        goto menu;
+        else
+        break;
+        case 2:
+        modification();
+          printf("click 1 pour retourne au menu \n click 2 pour finir le programme\n ");
+        printf("entre :");
+        scanf("%d",&h);
+        if(h==1)
+        goto menu;
+        else
+        break;
+        case 3:
+        suprimer();
+          printf("click 1 pour retourne au menu \n click 2 pour finir le programme\n ");
+        printf("entre :");
+        scanf("%d",&h);
+        if(h==1)
+        goto menu;
+        else
+        break;
+        case 4:
+        affichage();
+          printf("click 1 pour retourne au menu \n click 2 pour finir le programme\n ");
+        printf("entre :");
+        scanf("%d",&h);
+        if(h==1)
+        goto menu;
+        else
+        break;
+        case 5:
+        recherche();
+          printf("click 1 pour retourne au menu \n click 2 pour finir le programme\n ");
+        printf("entre :");
+        scanf("%d",&h);
+        if(h==1)
+        goto menu;
+        else
+        break;
+        case 6:
+          printf("click 1 pour retourne au menu \n click 2 pour finir le programme\n ");
+        printf("entre :");
+        scanf("%d",&h);
+        if(h==1)
+        goto menu;
+        else
+        break;
+        case 7:
 
+         printf("click 1 pour retourne au menu \n click 2 pour finir le programme\n ");
+        printf("entre :");
+        scanf("%d",&h);
+        if(h==1)
+        goto menu;
+        else
+        break;
+        case 8:
+        moyenne();
+        printf("click 1 pour retourne au menu \n click 2 pour finir le programme\n ");
+        printf("entre :");
+        scanf("%d",&h);
+        if(h==1)
+        goto menu;
+        else
+        break;
+       }
        return 0;
 }
