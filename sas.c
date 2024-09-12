f529bf376743e114f170ccf8c43d6a2a7f032a3d
imraneaitdahmade <imraneaitdahmade@gmail.com>
2024-09-12 15:32:53 +0100

    fini de projet sas

--- sas.c
+++ sas.c
@@ -13,7 +13,7 @@ typedef struct
 // declaration des variables global //
 eleve c[100];
  const char listdpar[6][6]={"MIP","PC","IN","BIO"};
-int q=0;
+int q=10;
 // la fonction d ajoute//
 void ajout()
 {
@@ -21,7 +21,7 @@ void ajout()
        int z,v;
        char w;
        // ecrire les donnees de etudiant //
-       q = 0;
+       
        do
        {
               printf("________________________________________________________________________________________________________\n");
@@ -41,7 +41,7 @@ void ajout()
               getchar();
               fgets(c[q].date, 20, stdin);
               // ajoute le departement d etudiant //
-              printf("entre votre departement ona 4 dapartement click le numero correspondant [1)MIP,2)PC,3)IN,4)BIO] :");
+              printf("entre votre departement ona 4 dapartement click le numero correspondant click 1(MIP),2(PC),3(IN),4(BIO) :");
               scanf("%d",&z);
               switch(z){
                      case 1:
@@ -176,14 +176,14 @@ void affichage(){
 int o;
 
 for(o=0;o<q;o++){
-     printf("les informationsde eleve id:%s \n",c[o].id);
+    // printf("les informations de eleve \nid:%s \n",c[o].id);
+     printf("le id est :%s \n",c[o].id);
      printf("le nom est :%s \n",c[o].nom);
      printf("le prenom est :%s \n",c[o].prenom);
      printf("le date de naissance est :%s \n",c[o].date);
      printf("la departement :%s \n",c[o].departement);
-     printf("le nombre total:%.2f\n",c[o].note);
-     
-}
+     printf("le nombre total:%.2f\n",c[o].note);  
+       }
 }
 //fonction de calculer la moyene general
 void moyenne(){
@@ -213,7 +213,7 @@ else if(p==1){
        if(strcmp(c[d].departement,moydepartement)==0){
               s=s+c[d].note;
               f++;
-              printf("%d",c[d].note);
+              printf("%f",c[d].note);
        }
        }
        M=s/f;
@@ -263,43 +263,169 @@ void recherche(){
        printf("le nombre total:%.2f\n",c[l].note);
        }
        }
-       break;
      }
        printf("________________________________________________________________________________________________________\n");
 }
 //fonction des statistiques
 void statistique(){
-      int t;
+      int t,a,y,j;
+      float e;
+      eleve temp;
+int p,m,i,b;p=0;m=0;i=0;b=0;
        printf("________________________________________________________________________________________________________\n");
-                     printf("_______________________________________ statistiques des informations  ______________________________________\n");
-                     printf("1)modif                                                                 2)modifier le prenom \n");
-                     printf("3)modifier le departement                                                         4)modifier la note generale \n");
-                     printf("                                        5)modifier la date de naissance \n");
-                     printf("entre :");
-                     scanf("%d",&t);
+       printf("_______________________________________ statistiques des informations  ______________________________________\n");
+       printf("1)Afficher le nombre total d'etudiants                                                     2)Afficher le nombre d'etudiants dans chaque departement \n");
+       printf("3)Afficher les etudiants ayant une moyenne generale superieure à un seuil                  4)Afficher les 3 etudiants ayant les meilleures notes. \n");
+       printf("                                 5)Afficher le nombre d'etudiants ayant reussi dans chaque departement \n");
+       printf("entre :");
+       scanf("%d",&t);
+       switch(t){
+              case 1:
+              printf("le nombre total des etudiants est %d",q);
+              break;
+              case 2:
+              for(a=0;a<=q;a++){
+                    if(strcmp(c[a].departement,"MIP")==0){
+                     m++;
+                    }
+                     if(strcmp(c[a].departement,"PC")==0){
+                     p++;
+                    } 
+                    if(strcmp(c[a].departement,"IN")==0){
+                     i++;
+                    }
+                     if(strcmp(c[a].departement,"BIO")==0){
+                     b++;
+                    }
+              }
+              printf("le nombre des etudiants dans MIP est :%d \n",m);
+              printf("le nombre des etudiants dans PC est :%d \n",p);
+              printf("le nombre des etudiants dans  IN est :%d \n",i);
+              printf("le nombre des etudiants dans BIO est :%d \n",b);
+              break;
+              case 3:
+              printf("entre le seuil");
+              scanf("%f",&e);
+              for(a=0;a<=q;a++){
+              if(c[a].note>=e){
+                     printf("%s | %s | %s | %s | %s | %f \n",c[a].id,c[a].nom,c[a].prenom,c[a].date,c[a].departement,c[a].note);
+              }
+              }
+              break;
+              case 4:
+              for(a=0;a<=q;a++){
+                    for(y=0;y<q-a-1;y++){
+                     if(c[y].note<c[y+1].note){
+                     temp=c[y];
+                     c[y]=c[y+1];
+                     c[y+1]=temp;
+                     }
+                    }
+              }
+               printf("les trois meilleure etudiants sont :\n");
+              for(j=0;j<3;j++){
+                     printf("%d) %s | %s | %s | %s | %s |%.2f \n",j+1,c[j].id,c[j].nom,c[j].prenom,c[j].date,c[j].departement,c[j].note);                  
+              }
+              break;
+              case 5:
+              for(a=0;a<q;a++){
+                 if(c[a].note>=10){
+
+                      if(strcmp(c[a].departement,"MIP")==0){
+                     m++;
+                    }
+                     if(strcmp(c[a].departement,"PC")==0){
+                     p++;
+                    } 
+                    if(strcmp(c[a].departement,"IN")==0){
+                     i++;
+                    }
+                     if(strcmp(c[a].departement,"BIO")==0){
+                     b++;
+                    }
+                 }
+              }
+              printf("le nombre des etudiants dans MIP est :%d \n",m);
+              printf("le nombre des etudiants dans PC est :%d \n",p);
+              printf("le nombre des etudiants dans  IN est :%d \n",i);
+              printf("le nombre des etudiants dans BIO est :%d \n",b);
+              break;
+       }
 }
 void tri(){
-       int i,j,e;
+       int i,j,e,t;
+       int a,y;
        eleve temp;
-       
-       for(i=0;i<q;i++){
-              for(j=0;j<q-i-1;j++){
-                     if(strcmp(c[j].nom,c[j+1].nom)>0){
+                     printf("________________________________________________________________________________________________________________\n");
+                     printf("__________________________________________________________ trier par :  ______________________________________\n");
+                     printf("1)Tri alphabetique des etudiants en fonction de leur nom                    2)Tri des etudiants par moyenne generale\n");
+                     printf("                                        3)Tri des etudiants selon leur statut de reussite                     \n");
+                     printf("entre :");
+                     scanf("%d",&t);
+                     switch(t){
+                            case 1:
+                      for(i=0;i<q;i++){
+                      for(j=0;j<q-i-1;j++){
+                      if(strcmp(c[j].nom,c[j+1].nom)>0){
                        temp=c[j];
                        c[j]=c[j+1];
                        c[j+1]=temp;
-                     }
+                      }
               }
        }
        for(e=0;e<q;e++){
-              printf("%s",c[e].nom);
+              printf("%d) %s | %s | %s | %s | %s | \n",e+1,c[e].id,c[e].nom,c[e].prenom,c[e].date,c[e].departement);
        }
+       break;
+       case 2:
+for(a=0;a<=q;a++){
+                    for(y=0;y<q-a-1;y++){
+                     if(c[y].note<c[y+1].note){
+                     temp=c[y];
+                     c[y]=c[y+1];
+                     c[y+1]=temp;
+                     }
+                    }
+              }
+               
+              for(j=0;j<q;j++){
+                     printf("%d) %s | %s | %s | %s | %s |%.2f \n",j+1,c[j].id,c[j].nom,c[j].prenom,c[j].date,c[j].departement,c[j].note);                  
+              }
+       break;
+       case 3:
+       for(a=0;a<=q;a++){
+                    for(y=0;y<q-a-1;y++){
+                     if(c[y].note<c[y+1].note){
+                     temp=c[y];
+                     c[y]=c[y+1];
+                     c[y+1]=temp;
+                     }
+                    }
+              }
+              for(j=0;j<q;j++){
+              if(c[j].note>=10){
+                     printf("%d) %s | %s | %s | %s | %s |%.2f \n",j+1,c[j].id,c[j].nom,c[j].prenom,c[j].date,c[j].departement,c[j].note); 
+              }
+
+              }
+       break;
+                     }
 
 }
 int main()
 {
         //declaration des variables local de main1
        int g,h;
+    c[0] = (eleve){"G134595302","Dupont", "Jean", "01/01/2000", 15.5, "MIP"};
+    c[1] = (eleve){"G134367543", "Martin", "Sophie", "02/02/1999", 17.0, "PC"};
+    c[2] = (eleve){"G134565654", "Durand", "Claire", "03/03/2001", 14.0, "IN"};
+    c[3] = (eleve){"G134345681", "Lemoine", "Paul", "04/04/2002", 16.0, "IN"};
+    c[4] = (eleve){"G134688754", "Leroy", "Anne", "05/05/2000", 18.5, "BIO"};
+    c[5] = (eleve){"G131247423", "Gauthier", "Louis", "06/06/1998", 12.5, "PC"};
+    c[6] = (eleve){"G134545689", "Moreau", "Isabelle", "07/07/2001", 3.0, "MIP"};
+    c[7] = (eleve){"G134535737", "Boucher", "eric", "08/08/2002", 19.0, "MIP"};
+    c[8] = (eleve){"G134595431", "Roux", "Nathalie", "09/09/1999", 10.0, "PC"};
+    c[9] = (eleve){"G134599875", "Pires", "Julien", "10/10/2000", 9.5, "BIO"};
        
        //menu principal//
        menu:
@@ -308,7 +434,7 @@ int main()
        printf("   1)ajouter un etudiant                                                        2)modifier les informations \n \n");
        printf("   3)suprimer un etudiant                                                       4)afficher les informations des etudiants \n \n");
        printf("   5)rechercher un etudiant                                                     6)Trier les etudiants \n \n");
-       printf("   7)statistiques                                                                8)calculer le moyenne general \n \n");
+       printf("   7)statistiques                                                               8)calculer le moyenne general \n \n");
        printf("______________________________________________________________________________________________________________\n");
        printf("entre :");
        scanf("%d",&g);
@@ -368,6 +494,7 @@ int main()
         else
         break;
         case 7:
+        statistique();
         printf("click 1 pour retourne au menu \n click 2 pour finir le programme\n ");
         printf("entre :");
         scanf("%d",&h);
